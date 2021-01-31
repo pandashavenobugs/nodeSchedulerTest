0a6461c5a895a12c71b5c18d5d65e1d4a82bc239
berat <cedinckan@gmail.com>
2021-01-31 14:25:59 +0300

    date with rule

--- app.js
+++ app.js
@@ -1,51 +1,27 @@
 const schedule = require('node-schedule');
 
-var timer1 = 0;
-var timer2 = 0;
+var date = new Date();
+console.log(date);
+// console.log(date.toLocaleString('tr-TR',{timeZone:'Europe/Istanbul'}))
+const year = date.getFullYear();
+const month = date.getMonth();
+const dateCurrent= date.getDate();
+const hour = date.getHours();
+console.log(hour);
 
-const rule = new schedule.RecurrenceRule();
-rule.dayOfMonth=25;
-// rule.hour=11;
-// rule.minute=26
-rule.tz= 'Europe/Istanbul'
+const minute = 10;
+const second=0;
+var rule = new schedule.RecurrenceRule();
+rule.year=year;
+rule.month=month;
+rule.date=dateCurrent;
+rule.hour=hour;
+rule.minute=minute;
+rule.second=second;
+rule.tz="Europe/Istanbul"
 
-
-
-var k = schedule.scheduleJob('deneme1','* * * * * *',()=>{
-  //console.log(` timer2: ${timer2}`);
-  timer2 +=1;
-  if(timer2 === 40){
-    schedule.cancelJob('deneme');
-  }
+console.log(rule);
+var x = schedule.scheduleJob('test',rule,async()=>{
+  console.log('scheduler has run');
+  console.log(date)
 })
-
-const name = 'deneme';
-//console.log(schedule.scheduledJobs);
-const jobs = schedule.scheduledJobs;
-const jobdDeneme = jobs['deneme12'];
-
-var j = schedule.scheduleJob('deneme',rule,()=>{
-  console.log(` timer1: ${timer1}`);
-  timer1 +=1;
-  j.cancel()
-  console.log(jobs[name])
-  
-  if(timer1 === 20){
-    schedule.cancelJob('deneme1');
-    
-    const x = schedule.scheduleJob('deneme1','* * * * * *',()=>{
-      console.log(` new timer2: ${timer2}`);
-      timer2 +=1;
-      if(timer2 === 40){
-        schedule.cancelJob('deneme');
-      }
-    })
-  }
-})
-
-console.log(jobs[name])
-try {
-  schedule.cancelJob('deneme12');
-} catch (error) {
-  console.log(error)
-}
